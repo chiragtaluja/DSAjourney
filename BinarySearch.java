8fd76db104fd2dfa4afc775be3716c3ff7c197eb
Chirag_taluja <chirag9111vansh@gmail.com>
2025-06-14 08:24:58 +0530

    14-06-2025

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,32 +1,32 @@
 package practice;
 
-public class BinarySearch {
-    public static int binarySearch(int[] arr, int target){
-        int start=0;
-        int end=arr.length-1;
-        if(arr.length==0){
-            return 0;
-        }
-        while(start<=end){
-            int mid=(start+end)/2;
-            if(arr[mid]<target){
-                start=mid+1;
-            }
-            else if(arr[mid]>target){
-                end=mid-1;
-            }
-            else{
+public class BinarySearch{
+    int binarySearch(int [] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + ((end - start) / 2);
+            if (arr[mid] == target) {
                 return mid;
+            }
+            if (arr[mid] > target) {
+                end = mid - 1;
+            }
+            if (arr[mid] < target) {
+                start = mid + 1;
             }
         }
         return -1;
     }
-
     public static void main(String[] args) {
-        int []arr={45 , 85 , 89 , 699 , 9669 };
-        int target=699;
-
-        int res=binarySearch(arr,target);
-        System.out.println(res);
-    }
+        BinarySearch bs = new BinarySearch();
+        int[] arr = {1, 3, 5, 7, 9, 11};
+        int target = 7;
+        int result = bs.binarySearch(arr, target);
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found.");
+        }
+}
 }
